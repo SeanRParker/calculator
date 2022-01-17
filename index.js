15092a3d47473b8bbce9db80227eee24ee5ac012
Shaws <seanrichardparker@gmail.com>
2022-01-17 15:16:40 -0500

    wire up clear btn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("JS hooked UP!")
+
+// screen
+let display = document.getElementById('display');
+
+// clear
+let clearBtn = document.getElementById('clear');
+
+let clear = () => {
+  console.log('cleaR!')
+  display.innerText = '';
+};
+
+clearBtn.addEventListener('click', clear);
+
+
+
